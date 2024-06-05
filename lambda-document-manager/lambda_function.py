4a0c7ffcb6acebce376dc255abc259cd8c6e5fac
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-05 13:53:48 +0900

    add parent document retrieval

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -493,21 +493,7 @@ def load_document(file_type, key):
                 print('err_msg: ', err_msg)
                 # raise Exception ("Not able to load docx")   
     
-    texts = ""
-    if len(contents)>0:
-        new_contents = str(contents).replace("\n"," ") 
-        print('length: ', len(new_contents))
-        
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=100,
-            separators=["\n\n", "\n", ".", " ", ""],
-            length_function = len,
-        ) 
-
-        texts = text_splitter.split_text(new_contents) 
-                        
-    return texts
+    return contents
 
 # load a code file from s3
 def load_code(file_type, key):
