1290294232d6f6578f3d3202c03329ee4013e56a
Kyoungsu Park <kyopark2014@gmail.com>
2024-07-02 23:51:55 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -563,11 +563,11 @@ def extract_images_from_pdf(reader, key):
         img.save(pixels, "png")
         pixels.seek(0, 0)
         
-        fname = 'pdfimg_'+key.split('/')[-1].split('.')[0]    
+        fname = 'img_'+key.split('/')[-1].split('.')[0]    
         img.seek(0, 0)
         response = s3_client.put_object(
             Bucket=s3_bucket,
-            Key='photo/'+fname+'.png',
+            Key='capture/'+fname+'.png',
             ContentType='image/png',
             Body=pixels
         )
