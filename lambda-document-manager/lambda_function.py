196152c033708cb7e34c71c3d0ca622cba40bb28
Kyoungsu Park <kyopark2014@gmail.com>
2024-07-05 08:18:59 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -781,10 +781,10 @@ def load_document(file_type, key):
                         Bucket=s3_bucket,
                         Key='capture/'+fname+'.jpg',
                         #ContentType='image/png',
-                        #Metadata = {
-                        #    "name": fname,
-                        #    "page": i
-                        #},
+                        Metadata = {
+                            "type": 'jpg',
+                            "page": i
+                        },
                         Body=img
                     )
                     print('response: ', response)
